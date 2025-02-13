dd2170f7c07514431224fbdaa822f60933f86c71
Sreyaes <sreyaes.csb2226@saintgits.org>
2025-02-13 15:53:40 +0530

    database

--- collegeconnect/collegeconnect/settings.py
+++ collegeconnect/collegeconnect/settings.py
@@ -79,6 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+         "USER": "root",
+        "PASSWORD": "Root12345",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
