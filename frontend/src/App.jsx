2fc6dc18b8840c571077ae150275db08bed48e42
Sethunath A <148645477+sethunath2003@users.noreply.github.com>
2025-05-14 00:26:11 +0530

    Merge branch 'branchmain2'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import axios from "axios";
 // Import components
-import Layout from "./components/Layout";
 import About from "./pages/About"
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
@@ -79,7 +78,7 @@ function App() {
   return (
     <Router>
       <div className="min-h-screen flex flex-col bg-gray-900">
-        <Layout />
+        <Navigation />
         <Routes>
           <Route path="/homepage" element={<Homepage />} />
           <Route path="/" element={<LandingPage />} />
